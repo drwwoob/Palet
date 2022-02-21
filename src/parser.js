0aac7a956a5dcdc0c8ca246dbd0309fd05ffb6a4
Julia Smith <juliasmith@Julias-MacBook-Pro.local>
2022-02-20 19:46:44 -0800

    tentatively working without tests

--- src/parser.js
+++ src/parser.js
@@ -26,6 +26,7 @@ function addToPalette(id, currentColor, tokenStream, swatchCount) {
     });
     currentColor = tokenStream.next().value;
   }
+
   return currentColor;
 }
 
@@ -36,15 +37,14 @@ function pushAssignment(target, operand1, operator, operand2) {
 }
 
 function parseStatements(tokenStream) {
-  let nextColor = tokenStream.next().value;
+  let colorA = tokenStream.next().value;
 
-  while (nextColor != undefined) {
-    let swatchA = swatches.get(nextColor);
+  while (colorA != undefined) {
+    let swatchA = swatches.get(colorA);
 
     if (swatchA == undefined) {
-      let colorA = nextColor;
-      nextColor = tokenStream.next().value;
-      let swatchB = swatches.get(nextColor);
+      let colorB = tokenStream.next().value;
+      let swatchB = swatches.get(colorB);
 
       if (swatchB == undefined) {
         let newPaletteID = "P" + palettes.size;
@@ -59,110 +59,131 @@ function parseStatements(tokenStream) {
         statements.push(new Assignment(newPaletteID, 0));
 
         //define new swatchess and go back to top
-        nextColor = addToPalette(newPaletteID, nextColor, tokenStream, 1);
+        colorA = addToPalette(newPaletteID, colorB, tokenStream, 1);
+      } else {
+        //swatchB is defined
+        //back to top without consuming colorB
+        colorA = colorB;
       }
-      //else back to top without consuming swatchB
     } else if (swatchA.operator != "*") {
       let operand = swatchA.palette;
       switch (swatchA.operator) {
         case "P": //print
           statements.push(new Call("print", operand));
+          break;
         case "+": //++
           pushAssignment(operand, operand, "+", 1);
+          break;
         case "-": //--
           pushAssignment(operand, operand, "-", 1);
+          break;
         case "j": //jmp
           statements.push(new Call("goto", operand));
+          break;
       }
       //back to top
-      nextColor = tokenStream.next().value;
+      colorA = tokenStream.next().value;
     } else {
       //swatchA.operator == "*"
       let operandA = swatchA.palette;
       let paletteA = palettes.get(operandA);
 
       //look at next color
-      nextColor = tokenStream.next().value;
-      console.log(nextColor);
-      if (nextColor != undefined) {
-        let swatchB = swatches.get(nextColor);
+      let colorB = tokenStream.next().value;
+      if (colorB != undefined) {
+        let swatchB = swatches.get(colorB);
 
         if (swatchB == undefined) {
           let swatchCount = paletteA.swatches;
           if (swatchCount < 5) {
             //define new swatches and go back to top
-            nextColor = addToPalette(
-              paletteA,
-              swatchB,
-              tokenStream,
-              swatchCount
-            );
+            colorA = addToPalette(paletteA, swatchB, tokenStream, swatchCount);
           } else {
             statements.push(new Call("print", swatchA.palette));
             //print
-            //back to top without consuming swatchB
+            //back to top without consuming colorB
+            colorA = colorB;
           }
         } else {
           //swachB is defined
           let operandB = swatchB.palette;
-
           if (swatchB.operator != "P") {
             switch (swatchB.operator) {
               case "+": //+=
                 pushAssignment(operandA, operandA, "+", operandB);
+                break;
               case "-": //-=
                 pushAssignment(operandA, operandA, "-", operandB);
+                break;
               case "j": //if jmp
-                statements.push(new Call("goto", [operandA, operandB]));
+                statements.push(new Call("gotoIf", [operandA, operandB]));
+                break;
             }
             // back to top
-            nextColor = tokenStream.next().value;
+            colorA = tokenStream.next().value;
           } else {
             //swatchB.operator == "P"
             //look at next color
-            nextColor = tokenStream.next().value;
-            if (nextColor != undefined) {
-              let swatchC = swatches.get(nextColor);
+            let colorC = tokenStream.next().value;
 
+            if (colorC != undefined) {
+              let swatchC = swatches.get(colorC);
               if (swatchC != undefined) {
                 let operandC = swatchC.palette;
                 switch (swatchC.operator) {
                   case "+": //+
                     pushAssignment(paletteA, operandB, "+", operandC);
+                    break;
                   case "-": //-
                     pushAssignment(paletteA, operandB, "-", operandC);
+                    break;
                   case "*": //*
                     pushAssignment(paletteA, operandB, "*", operandC);
+                    break;
                   case "j": ///
                     pushAssignment(paletteA, operandB, "/", operandC);
+                    break;
                 }
                 //back to top
-                nextColor = tokenStream.next().value;
+                colorA = tokenStream.next().value;
               } else {
                 //current swatchC is undefined
-                //look at next swatch and make it swatchC
-                nextColor = tokenStream.next().value;
-                let swatchC = swatches.get(nextColor);
+                //look at next swatch and make it new swatchC
+                colorC = tokenStream.next().value;
+                let swatchC = swatches.get(colorC);
 
                 if (swatchC != undefined) {
                   let operandC = swatchC.palette;
                   switch (swatchC.operator) {
                     case "*": //**
                       pushAssignment(paletteA, operandB, "^", operandC);
+                      break;
                     case "j": //%
                       pushAssignment(paletteA, operandB, "%", operandC);
+                      break;
                     case "+": //nothing yet
+                      break;
                     case "-": //nothing yet
+                      break;
                   }
                   //back to top
-                  nextColor = tokenStream.next().value;
+                  colorA = tokenStream.next().value;
                 }
-                //back to top without consuming swatchC
+                //back to top without consuming colorC
+                colorA = colorC;
               }
             }
+            //*P followed by nothing
+            //back to top without consuming colorC
+            //will exit loop
+            colorA = colorC;
           }
         }
       }
+      //* followed by nothing
+      //back to top without consuming colorB
+      //will exit loop
+      colorA = colorB;
     }
   }
 }
